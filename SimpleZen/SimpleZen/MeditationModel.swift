3dcb005697adaeefb73e9955a408534fc61d2ebc
vtboyarc <adamrcarter@gmail.com>
2016-01-21 20:08:22 -0600

    Created data model for meditations

--- SimpleZen/SimpleZen/MeditationModel.swift
+++ SimpleZen/SimpleZen/MeditationModel.swift
@@ -11,7 +11,16 @@ import GameKit
 struct MeditationModel {
     let meditations = [
         "There is nowhere to arrive except the present moment. - Thic Nhat Hanh",
-        "Your worst enemy cannot harm you as much as your own thoughts, unguarded. - Buddha"]
+        "Your worst enemy cannot harm you as much as your own thoughts, unguarded. - Buddha",
+        "Peace comes from within. Do not seek it without. – Buddha",
+        "There are two mistakes one can make along the road to truth: not going all the way, and not starting. – Buddha",
+        "The soul always knows what to do to heal itself. The challenge is to silence the mind. – Caroline Myss",
+        "Do not let the behavior of others destroy your inner peace. – Dalai Lama",
+        "If you cultivate the attitude of indifference towards the mind, gradually you will cease to identify with it. – Annamalai Swami",
+        "Mental problems feed on the attention that you give them. The more you worry about them, the stronger they become. If you ignore them, they lose their power and finally vanish. – Annamalai Swami",
+        "Be here now. Be someplace else later. Is that so complicated? – David M. Bader",
+        "Prayer is when you talk to God; meditation is when you listen to God. – Diana Robinson"
+    ]
     
     func getRandomMeditation() -> String {
         // GKRandomSource comes from the GameKit import
