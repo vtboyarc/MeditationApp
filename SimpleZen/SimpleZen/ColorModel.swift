b8465e08ecc8fd85966ecbbfefacfab9e61e0cb8
vtboyarc <adamrcarter@gmail.com>
2016-01-22 22:37:32 -0600

    Added colors, and app icons

--- SimpleZen/SimpleZen/ColorModel.swift
+++ SimpleZen/SimpleZen/ColorModel.swift
@@ -11,12 +11,13 @@ import GameKit
 
 struct ColorModel {
     let colors = [
-        UIColor(red: 192/255.0, green: 41/255.0, blue: 66/255.0, alpha: 1.0),
+        UIColor(red: 206/255.0, green: 183/255.0, blue: 149/255.0, alpha: 1.0),
         UIColor(red: 217/255.0, green: 91/255.0, blue: 67/255.0, alpha: 1.0),
         UIColor(red: 84/255.0, green: 36/255.0, blue: 55/255.0, alpha: 1.0),
         UIColor(red: 89/255.0, green: 79/255.0, blue: 79/255.0, alpha: 1.0),
         UIColor(red: 84/255.0, green: 121/255.0, blue: 128/255.0, alpha: 1.0),
-        UIColor(red: 69/255.0, green: 173/255.0, blue: 168/255.0, alpha: 1.0)
+        UIColor(red: 69/255.0, green: 173/255.0, blue: 168/255.0, alpha: 1.0),
+        UIColor(red: 152/255.0, green: 121/255.0, blue: 119/255.0, alpha: 1.0)
     ]
     
     func getThatRandomColorTho() -> UIColor {
