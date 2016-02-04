364cce18ca413b0a031858056ed229c31fa94ea5
vtboyarc <adamrcarter@gmail.com>
2016-02-03 21:44:32 -0600

    Version 1.2. Added new colors and quotes

--- SimpleZen/SimpleZen/ColorModel.swift
+++ SimpleZen/SimpleZen/ColorModel.swift
@@ -17,7 +17,13 @@ struct ColorModel {
         UIColor(red: 89/255.0, green: 79/255.0, blue: 79/255.0, alpha: 1.0),
         UIColor(red: 84/255.0, green: 121/255.0, blue: 128/255.0, alpha: 1.0),
         UIColor(red: 69/255.0, green: 173/255.0, blue: 168/255.0, alpha: 1.0),
-        UIColor(red: 152/255.0, green: 121/255.0, blue: 119/255.0, alpha: 1.0)
+        UIColor(red: 152/255.0, green: 121/255.0, blue: 119/255.0, alpha: 1.0),
+        //added below new colors for version 1.2, 4 new
+        UIColor(red: 71/255.0, green: 79/255.0, blue: 97/255.0, alpha: 1.0),
+        UIColor(red: 185/255.0, green: 138/255.0, blue: 119/255.0, alpha: 1.0),
+        UIColor(red: 65/255.0, green: 82/255.0, blue: 117/255.0, alpha: 1.0),
+        UIColor(red: 111/255.0, green: 96/255.0, blue: 108/255.0, alpha: 1.0)
+        
     ]
     
     func getThatRandomColorTho() -> UIColor {
